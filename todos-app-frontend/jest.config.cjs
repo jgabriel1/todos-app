97a9bd3b697777ebb98fdac094e07dcb84c63131
jgabriel1 <jotag.94@gmail.com>
2023-02-27 00:37:52 -0300

    feat(frontend): add tests setup

---
+++ todos-app-frontend/jest.config.cjs
@@ -0,0 +1,13 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  testMatch: [
+    '**/__tests__/**/?(*.)+(spec|test).[jt]s?(x)',
+    '**/?(*.)+(spec|test).[jt]s?(x)',
+  ],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+};
